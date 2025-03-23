a2fea526e2d0ceb5acde0b90a886b06f15348d54
enocol <eno_col@yahoo.com>
2025-03-23 07:27:14 +0000

    Fix error

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c0(*%9=1z0qb%hnh6==d8@a#!a%(8@e*@yw5xlo9-suuvj#5jn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
