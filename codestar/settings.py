2a1fdb647202246d72a905eda7b85671e81712dd
enocol <eno_col@yahoo.com>
2025-03-23 07:46:59 +0000

    make blog_view home page

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ih(z3c8i&xgp61wf5mt1+w#!o=xyf*e_i)ky7w0c3__ts!a#w7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuapp.com', '127.0.0.1', ]
 
 
 # Application definition
