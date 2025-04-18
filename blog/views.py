84a4243c839c0397426c54a11ecefd20aadf2557
enocol <eno_col@yahoo.com>
2025-04-18 04:34:07 +0100

    Add static files and post details view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -9,5 +10,31 @@ class PostList(generic.ListView):
     queryset = Post.objects.all()
     template_name = "blog/index.html"
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    # If the post is not found, render a 404 page
+
+
+    return render(
+        request,
+        "blog/post_details.html",
+        {"post": post},
+    )
 
 
--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,2 +1,8 @@
-console.log("Hello, World!");
-// This is a simple JavaScript file that logs "Hello, World!" to the console.
+const nav = document.querySelector(".navbar");
+nav.style.position = "fixed";
+nav.style.top = "0";
+nav.style.width = "100%";
+nav.style.zIndex = "1000";
+
+nav.style.transition = "background-color 0.3s ease-in-out";
+nav.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
